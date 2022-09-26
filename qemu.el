c87a0674cb3c248e24f17fb78f61e348a97dd14d
Arian-D <ArianXDehghani@gmail.com>
2022-09-26 05:08:37 -0700

    Add network and hdd flags

--- qemu.el
+++ qemu.el
@@ -5,7 +5,7 @@
    (list (transient-args 'qemu)))
   ;; Consider `start-process' in the future
   (async-shell-command
-   (cl-reduce (lambda (x y) (concat x " " y))
+   (cl-reduce (lambda (x y) (format "%s %s" x y))
 	      (cons "qemu-system-x86_64"
 		    args))
    "*qemu*"))
@@ -19,37 +19,57 @@
   ;; TODO: Add number and unit reader (M, G, etc)
   )
 
-
 (transient-define-argument qemu:-smp ()
-  :description "CPU cores"
+  :description "CPU SMP"
   :class 'transient-option
   :shortarg "-s"
   :argument "-smp "
   ;; TODO: add number reader
   )
 
-
 (transient-define-argument qemu:-cdrom ()
-  :description "CD-ROM image"
+  :description "CD-ROM (iso, img, ...)"
   :class 'transient-option
   :shortarg "-c"
   :argument "-cdrom "
-  :reader (lambda (&rest args) (interactive) (read-file-name "-cdrom → ")))
+  :reader (lambda (&rest args)
+	    (interactive)
+	    (read-file-name "-cdrom → ")))
+
+(transient-define-argument qemu:-nic ()
+  :description "Network"
+  :class 'transient-option
+  :shortarg "-n"
+  :argument "-nic ")
+
+(transient-define-argument qemu:-hda ()
+  :description "Hard disk image (qcow2, raw, ...)"
+  :class 'transient-option
+  :shortarg "-h"
+  :argument "-hda "
+  :reader (lambda (&rest args)
+	    (interactive)
+	    (read-file-name "-cdrom → ")))
+
 
 (transient-define-prefix qemu ()
   "A qemu[-kvm] interface for Emacs"
   :value (list "-enable-kvm"
 	       "-m 8G" ;; TODO: Change to system ram / 2
 	       "-smp 4" ;; TODO: Change to system cores / 2
+	       "-nic user"
 	       ;; TODO: Guess the default option based on extension
 	       )
   ["Arguments"
    (qemu:-m)
    (qemu:-smp)
+   (qemu:-nic)
    (qemu:-cdrom)
+   (qemu:-hda)
+
    ("-k" "Enable KVM" "-enable-kvm")]
   ["Run"
-   ("v" "Virtualize (qemu-kvm)" qemu-run)
+   ("v" "Virtualize" qemu-run)
    ;; ("e" "Emulate other architectures (TODO)" nil)
    ])
 
